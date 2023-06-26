3bc48d7af2ed838e11f47d14c2f4cbea0cf0f246
cynthiaaguero <cynthiaaguero02@gmail.com>
2023-06-25 20:42:41 -0400

    erorr uh oh

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@ import './App.css';
 import React from 'react';
 
 function App() {
+  const [question, setQuestion] = useState(1);
+
+  const handleNextQuestion = () => {
+    setQuestion(question + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          BiteBuddy      
-        </p>
+        <p>BiteBuddy</p>
       </header>
       <div className='App-body'>
-        <p>
-          Where are you?
-        </p>
-        <p>
-          <button className='myButton'>Your mom's house</button>
-          <button className='myButton'>Your dad's house</button>
-          <button className='myButton'>Your granny's house</button>
-          <button className='myButton'>Your dog's house</button>
-        </p>
+        {question === 1 && (
+          <>
+            <p>Where are you?</p>
+            <p>
+              <button className='myButton' onClick={handleNextQuestion}>Your mom's house</button>
+              <button className='myButton' onClick={handleNextQuestion}>Your dad's house</button>
+              <button className='myButton' onClick={handleNextQuestion}>Your granny's house</button>
+              <button className='myButton' onClick={handleNextQuestion}>Your dog's house</button>
+            </p>
+          </>
+        )}
         </div>
     </div>
   );
