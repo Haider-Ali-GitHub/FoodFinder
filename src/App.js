6617035dc0ec50695963ffc5a9bb4c6739291903
cynthiaaguero <cynthiaaguero02@gmail.com>
2023-06-25 20:45:57 -0400

    more buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
   const [question, setQuestion] = useState(1);
@@ -15,15 +15,21 @@ function App() {
       </header>
       <div className='App-body'>
         {question === 1 && (
-          <>
+          <div>
             <p>Where are you?</p>
             <p>
               <button className='myButton' onClick={handleNextQuestion}>Your mom's house</button>
-              <button className='myButton' onClick={handleNextQuestion}>Your dad's house</button>
-              <button className='myButton' onClick={handleNextQuestion}>Your granny's house</button>
-              <button className='myButton' onClick={handleNextQuestion}>Your dog's house</button>
             </p>
-          </>
+          </div>
+        )}
+        {question === 2 && (
+          <div>
+            <p>Surprise me or question game?</p>
+            <p>
+              <button className='myButton' onClick={handleNextQuestion}>Surprise me</button>
+              <button className='myButton' onClick={handleNextQuestion}>Your dad's house</button>
+            </p>
+          </div>
         )}
         </div>
     </div>
