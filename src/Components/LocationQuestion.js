f1d952a7a222ef22065d64e7e0b476fdcbcd3899
Haider Ali <haider.ali07304@gmail.com>
2023-06-29 11:05:21 -0400

    LOCATION

--- src/Components/LocationQuestion.js
+++ src/Components/LocationQuestion.js
@@ -4,10 +4,12 @@ import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
 const LocationQuestion = ({ handleLocationSelection, handleNextQuestion, location }) => {
   const containerStyle = {
     width: '100%',
-    height: '100%',
+    height: '400px', // Increase the height as per your requirement
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    borderRadius: '10px', // Make the container rounder
+    overflow: 'hidden', // Ensure content within the container stays contained
   };
   const center = {
     lat: 0,
@@ -44,11 +46,23 @@ const LocationQuestion = ({ handleLocationSelection, handleNextQuestion, locatio
   return (
     <div>
       <p>Where are you?</p>
-      <input id="autocomplete" ref={autocompleteInputRef} type="text" placeholder="Search Location"/>
-      <div>
+      <input
+        id="autocomplete"
+        ref={autocompleteInputRef}
+        type="text"
+        placeholder="Search Your Location For the Best Food Spots Near You!"
+        style={{
+          width: '50%',
+          height: '40px', // Increase the height as per your requirement
+          borderRadius: '200px', // Make the input rounder
+          padding: '20px', // Add some padding for better appearance
+          fontSize: '16px', // Adjust the font size as per your preference
+        }}
+      />
+      <div style={containerStyle}>
         <LoadScript googleMapsApiKey="AIzaSyAlDrtrrBsJ2p0JIP1Q0EQ5KJA5Q_DbiLg" libraries={["places"]} onLoad={handleApiLoad}>
-          <GoogleMap
-            mapContainerStyle={containerStyle}
+          { <GoogleMap
+            mapContainerStyle={{ width: '50%', height: '100%' }}
             center={center}
             zoom={10}
             onClick={(e) =>
@@ -59,7 +73,7 @@ const LocationQuestion = ({ handleLocationSelection, handleNextQuestion, locatio
             }
           >
             {location && <Marker position={location} />}
-          </GoogleMap>
+          </GoogleMap> } 
         </LoadScript>
       </div>
       {/* <button className="myButton" onClick={handleNextQuestion}>
