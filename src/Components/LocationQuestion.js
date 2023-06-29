23d0b053cb82d0d983a8f60cefb4695b72becedc
Haider Ali <haider.ali07304@gmail.com>
2023-06-29 11:46:41 -0400

    GO!

--- src/Components/LocationQuestion.js
+++ src/Components/LocationQuestion.js
@@ -19,6 +19,7 @@ const LocationQuestion = ({ handleLocationSelection, handleNextQuestion, locatio
   const autocompleteInputRef = useRef(null);
   const [autocomplete, setAutocomplete] = useState(null);
   const [selectedLocation, setSelectedLocation] = useState(null);
+  const [isLocationEntered, setIsLocationEntered] = useState(false);
 
   useEffect(() => {
     if (autocomplete) {
@@ -44,34 +45,64 @@ const LocationQuestion = ({ handleLocationSelection, handleNextQuestion, locatio
     setAutocomplete(autocompleteObject);
   };
 
-  const handleConfirmLocation = () => {
+  const handleLocationInputChange = () => {
+    const inputText = autocompleteInputRef.current.value;
+    setIsLocationEntered(inputText.trim().length > 0);
+  };
+
+  const handleGoButtonClick = () => {
     if (selectedLocation) {
       handleLocationSelection(selectedLocation);
       handleNextQuestion();
+    } else {
+      window.alert('Please select a valid location');
     }
   };
 
   return (
     <div>
       <p>Where are you?</p>
-      <input
-        id="autocomplete"
-        ref={autocompleteInputRef}
-        type="text"
-        placeholder="Search Your Location For the Best Food Spots Near You!"
-        style={{
-          width: '50%',
-          height: '40px', // Increase the height as per your requirement
-          borderRadius: '200px', // Make the input rounder
-          padding: '20px', // Add some padding for better appearance
-          fontSize: '16px', // Adjust the font size as per your preference
-        }}
-      />
+      <div style={{ position: 'relative' }}>
+        <input
+          id="autocomplete"
+          ref={autocompleteInputRef}
+          type="text"
+          placeholder="Search Your Location For the Best Food Spots Near You!"
+          style={{
+            width: '50%',
+            height: '40px', // Increase the height as per your requirement
+            borderRadius: '200px', // Make the input rounder
+            padding: '20px', // Add some padding for better appearance
+            fontSize: '16px', // Adjust the font size as per your preference
+            paddingRight: '70px', // Add space for the button
+          }}
+          onChange={handleLocationInputChange}
+        />
+        {isLocationEntered && (
+          <button
+            style={{
+              position: 'absolute',
+              top: '50%',
+              right: '430px',
+              transform: 'translateY(-50%)',
+              width: '50px',
+              height: '50px',
+              borderRadius: '50%',
+              background: 'lightblue',
+              border: 'none',
+              cursor: 'pointer',
+            }}
+            onClick={handleGoButtonClick}
+          >
+            GO!
+          </button>
+        )}
+      </div>
       <div style={containerStyle}>
         <LoadScript googleMapsApiKey="AIzaSyAlDrtrrBsJ2p0JIP1Q0EQ5KJA5Q_DbiLg" libraries={["places"]} onLoad={handleApiLoad}>
           {selectedLocation && (
             <GoogleMap
-              mapContainerStyle={{ width: '50%', height: '100%' }}
+              mapContainerStyle={{ width: '55%', height: '100%' }}
               center={selectedLocation}
               zoom={16}
               onClick={(e) =>
@@ -86,9 +117,6 @@ const LocationQuestion = ({ handleLocationSelection, handleNextQuestion, locatio
           )}
         </LoadScript>
       </div>
-      <button className="myButton" onClick={handleConfirmLocation}>
-        Confirm Location
-      </button>
     </div>
   );
 }
