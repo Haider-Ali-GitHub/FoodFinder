ec346640bc46add4d73db555d34870ca93b84f01
cynthiaaguero <cynthiaaguero02@gmail.com>
2023-08-03 20:14:49 -0400

    miles to meters

--- src/Components/DistanceQuestion.js
+++ src/Components/DistanceQuestion.js
@@ -3,14 +3,14 @@ import React from "react";
 function DistanceQuestion({handleNextQuestion}) {
 return(
     <div>
-        <p>How far do you want to go?</p>
-        <p>
+      <p>How far do you want to go?</p>
+      <p>
             <button className='myButton' onClick={handleNextQuestion}>5 mi</button>
             <button className='myButton' onClick={handleNextQuestion}>10 mi</button>
             <button className='myButton' onClick={handleNextQuestion}>15 mi</button>
             <button className='myButton' onClick={handleNextQuestion}>20 mi</button>
             <button className='myButton' onClick={handleNextQuestion}>Surprise me</button>
-        </p>
+      </p>
     </div>
 )}
 
